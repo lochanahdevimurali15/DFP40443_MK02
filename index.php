1fc804985402e81946b60d5f761f865d2c3de406
lochanahdevimurali15 <lochanahdevi@gmail.com>
2026-01-20 11:25:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$nama = "LOCHANAHDEVI MURALI";
+$nomatrik = "18DIT24F1022";
+$kelas = "DIT4A";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Profile</title>
+</head>
+<body>
+    <h2>My Profile</h2>
+    <p>Name: <?= $nama ?></p>
+    <p>Matric No: <?= $nomatrik ?></p>
+    <p>Class: <?= $kelas ?></p>
+    <img src="photo.jpg" width="300">
+</body>
+</html>
